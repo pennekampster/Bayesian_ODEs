0b1ee3c0f3687d44ca52165c7ec4e822b32fdf06
Lukas <mail@heiland.earth>
2021-08-16 23:59:54 +0200

    add stan model

---
+++ FRT.stan
@@ -0,0 +1,79 @@
+functions {
+  
+  //// The ODE system. See https://mc-stan.org/docs/2_27/functions-reference/functions-ode-solver.html
+  vector x_t(real time, vector x,
+             real b, real c, real h, real K, real q, real r) {
+
+    vector[2] dx_dt;
+    
+    // Prey:
+    dx_dt[1] = -b * pow(x[1], 1+q) / (1 + b * h * pow(x[1], 1+q)) * x[2] + r * x[1] * (1.0 - x[1]/K);
+    // Predators:
+    dx_dt[2] = x[2] * c * b * pow(x[1], 1+q) / (1 + b * h * pow(x[1], 1+q));
+
+    return dx_dt;
+  }
+
+}
+
+data {
+  
+  int<lower=0> N_series;
+  array[N_series, 1] real time_end;
+  array[N_series, 2] int x_start;
+  array[N_series, 2] int x_end;
+  
+}
+
+parameters {
+  
+  //// ODE parameters
+  real b_log;
+  real c_log;
+  real h_log;
+  real K_log;
+  real q;
+  real r_log;
+  
+  array[N_series] vector[2] x_hat_start_log;
+  
+  //// Statistical parameters
+  // real<lower=0> sigma;
+  
+}
+
+transformed parameters {
+  
+  array[N_series, 1] vector<lower=0>[2] x_hat_end;
+    // ode integrator expects an array[N_data,N_times] vector[N_states]. This is why some arrays have the superficial indices here: array[N_series, 1]
+  
+  for(s in 1:N_series) {
+      
+      x_hat_end[s,] = ode_rk45(x_t, exp(x_hat_start_log[s]), 0.0, time_end[s,],
+                      exp(b_log), exp(c_log), exp(h_log), exp(K_log), q, exp(r_log));
+  }
+}
+
+
+model {
+  // Priors
+  b_log ~ normal(-4.106669, 2);
+  c_log ~ normal(-5.600194, 2);
+  h_log ~ normal(-3.531813, 2);
+  K_log ~ normal(7.901859, 2);
+  q ~ normal(0.867401, 2);
+  r_log ~ normal(-0.994875, 2);
+  
+  // sigma ~ normal(0, 1);
+  
+
+  //// Statistical model
+  for(s in 1:N_series) {
+    
+    //// Fitiing the unknown true initial state to the data,
+    x_start[s,] ~ poisson(exp(x_hat_start_log[s])); //, sigma
+    
+    //// Fitiing the final state from the ODE to the data
+    x_end[s,] ~ poisson(x_hat_end[s, 1]);
+  }
+}
---
+++ FRT.stan.R
@@ -0,0 +1,44 @@
+# Library -----------------------------------------------------------------
+library(tidyverse)
+library(cmdstanr)
+## make sure to:
+# cmdstanr::install_cmdstan()
+
+
+# Load and prepare data ---------------------------------------------------------------
+
+D <- read_csv(here("data/FRT_Dataset.csv"))
+
+## returns a list with data structures as declared in "FRT.stan"
+getData <- function(D, temp = 20) {
+  D <- filter(D, Temperature == temp & Treatment == "predator")
+  data <- list(
+    N_series = nrow(D),
+    time_end = as.matrix(D["Incubation_time"]), # expects array[N_series, 1]
+    x_start = cbind(round(D$Prey_start_density), round(D$Predator_start_density)), # expects array[N_series] vector[2]
+    x_end = cbind(round(D$Prey_end_density), round(D$Predator_end_density)) # ??? Original code seems to: D$Prey_start_density - D$Prey_end_density, which leads to negative numbers
+  )
+}
+
+
+# Fit stan model ----------------------------------------------------------
+
+model <- cmdstan_model("FRT.stan")
+n_chains <- 3
+# if(!dir.exists("Draws")) dir.create("Draws")
+fit_25 <- model$sample(data = getData(D, temp = 25),
+                         init = 1, # init = replicate(n_chains, list(b_log = -4.106669, c_log = -5.600194, h_log = -3.531813, K_log = 7.901859, q = 0.867401, r_log = -0.994875), simplify = F) # sigma = 0.222916
+                         iter_warmup = 300, iter_sampling = 300, chains = n_chains, parallel_chains = n_chains, output_dir = "Draws", output_basename = "fit_25")
+
+# fit_25$save_output_files(dir = "Draws", basename = "fit_25")
+# fit_25 <- cmdstanr::read_cmdstan_csv(fit_25$output_files())
+
+# Explore fit ----------------------------------------------------------
+
+fit_25$summary()
+
+includepars <- c("b_log", "c_log","h_log", "K_log", "q", "r_log")
+draws_25 <- fit_25$draws()
+bayesplot::mcmc_trace(draws_25, pars = includepars)
+bayesplot::mcmc_areas(draws_25, area_method = "equal height", pars = includepars)
+
