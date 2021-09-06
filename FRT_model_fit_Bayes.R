dd97bdd3f820b48a583a83c793034f32cad6310d
Frank Pennekamp <pennekampster@gmail.com>
2021-09-06 13:40:15 +0200

    Added code to use a first MCMC run as prior; leads to much better convergence since we do not get problems with starting values

--- FRT_model_fit_Bayes.R
+++ FRT_model_fit_Bayes.R
@@ -32,13 +32,15 @@ FRT_Dataset_15 <- filter(FRT_Dataset, Temperature==15)
 FRT_Dataset_20 <- filter(FRT_Dataset, Temperature==20)
 FRT_Dataset_25 <- filter(FRT_Dataset, Temperature==25)
 
+
+
 ############## FUNCTIONS
 source("FRT_function_Bayes.R")
 
 ############## FITTING
 
 # | TEMPERATURE  15°C |
-
+FRT_Dataset <- FRT_Dataset_15
 refPars <- data.frame(best=c(-4.106669, -3.531813,  0.867401, -0.994875, 7.901859, -5.600194,  0.222916), 
                       lower = c(-20, -10, -0.99,  -10, 1,    -10, 0),
                       upper = c(5, 1,     3,   1, 10,    10, 1),
@@ -55,22 +57,36 @@ prior <- createUniformPrior(lower = refPars$lower,
                             upper = refPars$upper, 
                             best = refPars$best)
 
+#prior <- createTruncatedNormalPrior(refPars$best, rep(2,7), refPars$lower, refPars$upper)
+
 bayesianSetup <- createBayesianSetup(nll.odeint.general.pred, prior=prior, names=c("b.log", "h.log", "q", "r.log", "K.log", "c.log", "sigma"))
-iter = 10000
+iter = 1000
 settings = list(iterations = iter, message = F)
 
 out <- runMCMC(bayesianSetup = bayesianSetup, settings = settings)
 
-## Not run: 
-plot(out)
-summary(out)
-marginalPlot(out)
-correlationPlot(out)
-gelmanDiagnostics(out) # should be below 1.05 for all parameters to demonstrate convergence 
+# create prior from successful previous run
+newPrior = createPriorDensity(out, method = "multivariate",
+                              eps = 1e-10, lower = refPars$lower,
+                              upper = refPars$upper, best = refPars$best)
 
+bayesianSetup <- createBayesianSetup(nll.odeint.general.pred, prior=newPrior, names=c("b.log", "h.log", "q", "r.log", "K.log", "c.log", "sigma"))
+iter = 10000
+settings = list(iterations = iter, message = F)
+
+out_15 <- runMCMC(bayesianSetup = bayesianSetup, settings = settings)
+
+## Not run: 
+plot(out_15)
+summary(out_15)
+marginalPlot(out_15)
+correlationPlot(out_15)
+gelmanDiagnostics(out_15) # should be below 1.05 for all parameters to demonstrate convergence 
 
 
 # | TEMPERATURE  20°C |
+
+FRT_Dataset <- FRT_Dataset_20
 
 refPars <- data.frame(best=c(-15.229665, -3.100683,  1.250667, 0.136438, 7.897008, -4.554020,  0.236063), 
                       lower = c(-20, -10, -0.99,  -10, 1,    -10, 0),
@@ -88,17 +104,29 @@ prior <- createUniformPrior(lower = refPars$lower,
                             best = refPars$best)
 
 bayesianSetup <- createBayesianSetup(nll.odeint.general.pred, prior=prior, names=c("b.log", "h.log", "q", "r.log", "K.log", "c.log", "sigma"))
-iter = 10000
+iter = 1000
 settings = list(iterations = iter, message = F)
 
 out <- runMCMC(bayesianSetup = bayesianSetup, settings = settings)
 
+# create prior from successful previous run
+newPrior = createPriorDensity(out, method = "multivariate",
+                              eps = 1e-10, lower = refPars$lower,
+                              upper = refPars$upper, best = refPars$best)
+
+bayesianSetup <- createBayesianSetup(nll.odeint.general.pred, prior=newPrior, names=c("b.log", "h.log", "q", "r.log", "K.log", "c.log", "sigma"))
+iter = 10000
+settings = list(iterations = iter, message = F)
+
+out_20 <- runMCMC(bayesianSetup = bayesianSetup, settings = settings)
+
+
 ## Not run: 
-plot(out)
-summary(out)
-marginalPlot(out)
-correlationPlot(out)
-gelmanDiagnostics(out) # should be below 1.05 for all parameters to demonstrate convergence 
+plot(out_20)
+summary(out_20)
+marginalPlot(out_20)
+correlationPlot(out_20)
+gelmanDiagnostics(out_20) # should be below 1.05 for all parameters to demonstrate convergence 
 
 
 # | TEMPERATURE  25°C |
@@ -112,6 +140,8 @@ gelmanDiagnostics(out) # should be below 1.05 for all parameters to demonstrate 
 #                       row.names=c("b.log", "h", "q", "r", "K.log", "c", "sigma"))
 
 
+FRT_Dataset <- FRT_Dataset_25
+
 refPars <- data.frame(best=c(1, -6.987074,  0.86740, -1.662839, 8.476514, -4.580168,  0.42), 
                       lower = c(-20, -10, -0.99,  -10, 1,    -10, 0),
                       upper = c(5, 1,     3,   1,     10,    10, 1),
@@ -123,15 +153,28 @@ prior <- createUniformPrior(lower = refPars$lower,
                             best = refPars$best)
 
 bayesianSetup <- createBayesianSetup(nll.odeint.general.pred, prior=prior, names=c("b.log", "h.log", "q", "r.log", "K.log", "c.log", "sigma"))
-iter = 10000
-settings = list(iterations = iter, message = T)
+iter = 1000
+settings = list(iterations = iter, message = F)
 
 out <- runMCMC(bayesianSetup = bayesianSetup, settings = settings)
 
+# create prior from successful previous run
+newPrior = createPriorDensity(out, method = "multivariate",
+                              eps = 1e-10, lower = refPars$lower,
+                              upper = refPars$upper, best = refPars$best)
+
+bayesianSetup <- createBayesianSetup(nll.odeint.general.pred, prior=newPrior, names=c("b.log", "h.log", "q", "r.log", "K.log", "c.log", "sigma"))
+iter = 10000
+settings = list(iterations = iter, message = F)
+
+out_25 <- runMCMC(bayesianSetup = bayesianSetup, settings = settings)
+
+
+
 ## Not run: 
-plot(out)
-summary(out)
-marginalPlot(out)
-correlationPlot(out)
-gelmanDiagnostics(out) # should be below 1.05 for all parameters to demonstrate convergence 
+plot(out_25)
+summary(out_25)
+marginalPlot(out_25)
+correlationPlot(out_25)
+gelmanDiagnostics(out_25) # should be below 1.05 for all parameters to demonstrate convergence 
 
